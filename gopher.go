14b32bf46064ab72cb39ba4c8e8aa8de274239bf
Caio Barbieri <caiolvbarbieri@pm.me>
2022-06-20 13:56:44 -0300

    vars

--- gopher.go
+++ gopher.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-// Package Level: Scope
+// Scope: Package Level
 var y = "Hello, World!"
 
 func main() {
 
-	// Package Level: Code Block
+	// Scope: Code Block
 	// Declaração
 	x := 10
 
@@ -17,8 +17,12 @@ func main() {
 	fmt.Printf("x: %v, %T\n", x, x)
 	fmt.Printf("y: %v, %T\n", y, y)
 
-	// Atribuição
 	x, z := 20, 30
 	fmt.Printf("x: %v, %T\n", x, x)
 	fmt.Printf("z: %v, %T\n", z, z)
+
+	// Atribuição
+	z = 10
+	fmt.Printf("z: %v, %T\n", z, z)
+
 }
